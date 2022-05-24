8447597999034ab0e11aad28c54cf9024d8a0c50
Sulav <sulavshrestha023@gmail.com>
2022-05-24 11:09:33 +0545

    form addded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import Values from "values.js";
 function App() {
-  return <h1>Hello</h1>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <ssection className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button className="btn">generate</button>
+        </form>
+      </ssection>
+      <section className="colors">
+        <h4>List goes Here</h4>
+      </section>
+    </>
+  );
 }
 
 export default App;
